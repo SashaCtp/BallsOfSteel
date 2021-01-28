8a6718add71f659de2549e55bbac00252efbd7a0
Sasha Contrepois <sasha.contrepois@hotmail.fr>
2021-01-28 02:07:11 +0100

    🔧 Add @NotNull annotations

--- src/fr/rubyz/bos/commands/StatsCommand.java
+++ src/fr/rubyz/bos/commands/StatsCommand.java
@@ -1,19 +1,18 @@
 package fr.rubyz.bos.commands;
 
 import fr.rubyz.bos.BallsOfSteel;
-import fr.rubyz.bos.GameConfiguration;
 import fr.rubyz.bos.GameState;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.player.PlayerEditBookEvent;
+import org.jetbrains.annotations.NotNull;
 
 public class StatsCommand implements CommandExecutor {
 
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, String[] args) {
 
         // Display the sender's statistics
         if(args.length == 0){
